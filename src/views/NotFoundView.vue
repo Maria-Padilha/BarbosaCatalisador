06df13749a7d7e82326a1c3d4a1e2c92518d36a2
Maria-Padilha <padilhabarbosa23@gmail.com>
2025-11-18 17:16:59 -0400

    arrumando a página not found

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,31 +1,28 @@
 <template>
-  <v-main :class="darkMode.darkMode ? 'bg-dark-primary' : 'bg-light-primary'">
-    <main class="mt-10 w-[100%] h-[700px]">
+  <v-main class="bg-slate-950 text-slate-100">
+    <main class="mt-10 w-[100%] h-[90vh]">
       <section class="w-[100%] h-[100%] flex flex-col items-center justify-center">
-        <v-card width="250px" class="bg-transparent" elevation="0">
-            <v-img
-                height="130" :src="require('@/assets/img/logo.png')"
-                alt="logo-ícone maralua" cover
-            ></v-img>
-          </v-card>
-        <p class="text-5xl mt-3 font-bold text-[#fd8839]">404</p>
+        <v-card width="240px" class="bg-transparent" elevation="0">
+          <v-img
+              :src="require('@/assets/img/logo.png')"
+              alt="logo-ícone barbosa catalisador" cover
+          ></v-img>
+        </v-card>
 
-        <p class="text-2xl mt-7 text-center" :class="darkMode.darkMode ? ' text-[#ffa366]' : 'text-[#fd8839] font-bold'">
+        <p class="text-2xl mt-7 text-center font-bold text-amber">
           OPPS! Essa página não foi encontrada!
         </p>
         <p class="mt-4 md:text-lg text-center">
-          Volte para a página inicial e descubra como a
-          <span class="font-bold" :class="darkMode.darkMode ? ' text-[#ffa366]' : 'text-[#fd9836]'">maralua</span>
-          pode <br> eternizar o seu momento.
+          Volte para a página inicial e descubra como
+          <span class="font-bold text-[#00C94F]">Barbosa Catalisador</span>
+          pode <br> ajudar você a economizar e proteger o meio ambiente.
         </p>
-        <v-btn to="/" class="mt-7 w-[130px]" variant="tonal">Voltar</v-btn>
+        <v-btn to="/" class="mt-7 w-[130px] text-none" variant="flat" color="amber">Voltar</v-btn>
       </section>
     </main>
   </v-main>
 </template>
 
 <script setup>
-import {useDarkModeStore} from "@/stores/themes/darkmode";
 
-const darkMode = useDarkModeStore();
 </script>
